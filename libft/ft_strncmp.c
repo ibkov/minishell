8389aa92219072b71f4a7c0468dbdf5eb3bd6d27
burswyck <ibkov@yandex.ru>
2021-05-12 21:12:50 +0300

    Привет, я решил положить начало нашего пути в минишел. Сделал главные файлы и make рабочий. Пока не сделал вывод надписи minishell когда заходишь в программу. Если хочешь займись этим там просто. Не успеваю.

---
+++ libft/ft_strncmp.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: burswyck <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/12 20:12:12 by burswyck          #+#    #+#             */
+/*   Updated: 2020/11/12 20:12:15 by burswyck         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+	unsigned char	*str1;
+	unsigned char	*str2;
+
+	str1 = (unsigned char *)s1;
+	str2 = (unsigned char *)s2;
+	while ((*str1 || *str2) && n > 0)
+	{
+		if (*str1 != *str2)
+			return (*str1 - *str2);
+		str1++;
+		str2++;
+		n--;
+	}
+	return (0);
+}
